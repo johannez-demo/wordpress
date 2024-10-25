6ac2e9a901d4e6d1d7abd8281bc3c814f7f7dec2
Johannes Schmidt <mail@johannez.com>
2024-10-25 15:13:05 -0700

    feat: improved site install script

--- scripts/site_install.sh
+++ scripts/site_install.sh
@@ -4,15 +4,21 @@ VENDOR_DIR=$DOCROOT/vendor
 SCRIPTS_DIR=$DOCROOT/scripts
 WPCLI=$VENDOR_DIR/bin/wp
 
-$WPCLI --allow-root core install \
+echo ""
+echo "Running composer install..."
+echo ""
+composer install
+
+echo ""
+echo "Running wp cli core install..."
+echo ""
+${WPCLI} --allow-root core install \
 		--url=${WP_SITE_URL} \
-		--title=Demo \
+		--title=${WP_SITE_TITLE} \
 		--admin_user=${WP_ADMIN_USER} \
 		--admin_password=${WP_ADMIN_PASSWORD} \
 		--admin_email=${WP_ADMIN_EMAIL}
 
-$WPCLI rewrite structure ${WP_POST_URL_STRUCTURE}
-
-$WPCLI plugin activate advanced-custom-fields regenerate-thumbnails user-role-editor demo-config
-
-$WPCLI eval-file $SCRIPTS_DIR/acf/acf_import.php
+${WPCLI} rewrite structure ${WP_POST_URL_STRUCTURE}
+${WPCLI} plugin activate advanced-custom-fields regenerate-thumbnails user-role-editor demo-config
+${WPCLI} eval-file ${SCRIPTS_DIR}/acf/acf_import.php
